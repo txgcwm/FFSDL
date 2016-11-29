cdb7c806bf6bbf298fe72c9ebcd0c059c2d50d46
oujiangping <785992791@qq.com>
2016-11-28 17:55:41 -0800

    miss loopbuffer file

---
+++ LoopBuffer.h
@@ -0,0 +1,22 @@
+#ifndef __LOOPBUFFER__H__
+#define __LOOPBUFFER__H__
+
+#include <string.h>
+
+class LoopBuffer {
+    private:
+        unsigned char* buffer;
+        int maxSize;
+        int size;
+        int startPos;
+        int endPos;
+    public:
+        LoopBuffer(int size);
+        bool pushData(unsigned char* data, int len);
+        bool getData(unsigned char* data, int len);
+        int getSize();
+        int getMaxSize();
+        int getFree();
+};
+
+#endif
